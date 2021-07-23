9c4a606be9da241f66f55ea73f51eac5c16a23c5
DJ Drakos <dianajodrakos@gmail.com>
2021-07-22 18:35:51 -0700

    added failing GET test

--- __tests__/pizzas.test.js
+++ __tests__/pizzas.test.js
@@ -2,12 +2,12 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
+import Pizza from '../lib/models/Pizza.js';
 
 describe('demo routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-
 
   it('POSTS a new pizza', async () => {
     const pizza = { 
@@ -25,9 +25,26 @@ describe('demo routes', () => {
   });
 
 
-  // it('GETS all pizzas', () => {
+  it('GETS all pizzas', async () => {
+    const pizza1 = await Pizza.create({
+      name: 'hawaiian',
+      toppings: 'cheese, pineapple, canadian bacon',
+      rating: 8
+    });
 
-  // });
+    const pizza2 = await Pizza.create({
+      name: 'a better hawaiian',
+      toppings: 'cheese, pineapple, proscuitto, basil, kalamata olives',
+      rating: 11
+    });
+
+    return request(app)
+      .get('api/v1/pizzas')
+      .then((res) => {
+        expect(res.body).toEqual([pizza1, pizza2]);
+      });
+    
+  });
 
 
   // it('GETS one pizza by id', () => {
