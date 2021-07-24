b2612fd38b6e905c896945b3b33d6d13a2674caf
DJ Drakos <dianajodrakos@gmail.com>
2021-07-24 13:39:27 -0700

    added fruits table

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,20 +1,21 @@
+DROP TABLE IF EXISTS fruits;
 DROP TABLE IF EXISTS pizzas;
-DROP TABLE IF EXISTS rats;
 DROP TABLE IF EXISTS plants;
+DROP TABLE IF EXISTS rats;
 
+
+CREATE TABLE fruits (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name TEXT NOT NULL,
+    type TEXT NOT NULL,
+    season TEXT NOT NULL,
+    good_on_pizza BOOL NOT NULL
+)
 CREATE TABLE pizzas (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name TEXT NOT NULL,
     toppings TEXT NOT NULL,
     rating INTEGER NOT NULL
-);
-
-CREATE TABLE rats (
-    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    name TEXT NOT NULL,
-    size TEXT NOT NULL,
-    location TEXT NOT NULL,
-    likes_pizza BOOL NOT NULL
 );
 
 CREATE TABLE plants (
@@ -23,5 +24,13 @@ CREATE TABLE plants (
     scientific_name TEXT NOT NULL,
     light TEXT NOT NULL,
     difficulty INTEGER NOT NULL
+);
+
+CREATE TABLE rats (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name TEXT NOT NULL,
+    size TEXT NOT NULL,
+    location TEXT NOT NULL,
+    likes_pizza BOOL NOT NULL
 );
 
